05472b072ef64f23e8e98e3174ba57c787f330e2
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-03 22:41:55 +0100

    simplified actionparameters class as uch as possible

--- src/actionparameters.ts
+++ src/actionparameters.ts
@@ -2,36 +2,12 @@ import * as core from '@actions/core';
 import { Package } from 'azure-actions-utility/packageUtility';
 
 export class ActionParameters {
-    private static actionparams: ActionParameters;
-    private _packageInput: string;
-    private _package: Package;
-    private _publishProfileContent: string;
+    public package: Package;
+    public publishProfileContent: string;
 
-    private constructor() {
-        this._publishProfileContent = core.getInput('publish-profile');
-        this._packageInput = core.getInput('package');
-    }
-
-    public static getActionParams() {
-        if (!this.actionparams) {
-            this.actionparams = new ActionParameters();
-        }
-        return this.actionparams;
-    }
-
-    public get packageInput() {
-        return this._packageInput;
-    }
-
-    public get package() {
-        return this._package;
-    }
-
-    public set package(appPackage: Package) {
-        this._package = appPackage;
-    }
-
-    public get publishProfileContent() {
-        return this._publishProfileContent;
+    public constructor() {
+        this.publishProfileContent = core.getInput('publish-profile');
+        let packageInput = core.getInput('package');
+        this.package = new Package(packageInput);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import * as core from '@actions/core';
 
 import { ActionParameters } from "./actionparameters";
 import { WebAppDeployer } from './WebAppDeployer';
-import { Package } from 'azure-actions-utility/packageUtility';
 
 export async function main() {
   try {
     // Initialize action inputs
-    let actionParams = ActionParameters.getActionParams();
-    actionParams.package = new Package(actionParams.packageInput);
+    let actionParams = new ActionParameters();
     
     var deploymentProvider = new WebAppDeployer(actionParams);
     await deploymentProvider.DeployWebApp();
