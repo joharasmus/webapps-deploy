ce0a26c3c74ea1a3a825deaafefa234a7c3e3a76
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-03 01:31:41 +0100

    something is wrong, include zip and folder

--- src/WebAppDeploymentProvider.ts
+++ src/WebAppDeploymentProvider.ts
@@ -26,13 +26,18 @@ export class WebAppDeploymentProvider extends BaseWebAppDeploymentProvider {
         else {
             // Retains the old behavior of determining the package type from the file extension if valid type is not defined
             let packageType = appPackage.getPackageType();
-            switch(packageType){ 
+            switch(packageType){
                     
                 case PackageType.folder:
                     let tempPackagePath = utility.generateTemporaryFolderOrZipPath(`${process.env.RUNNER_TEMP}`, false);
                     webPackage = await zipUtility.archiveFolder(webPackage, "", tempPackagePath) as string;
                     core.debug("Compressed folder into zip " +  webPackage);
                     core.debug("Initiated deployment via kudu service for webapp package : "+ webPackage);
+                    this.actionParams.type = "zip";
+                    break;
+                    
+                case PackageType.zip:
+                    core.debug("Initiated deployment via kudu service for webapp zip package : "+ webPackage);
                     this.actionParams.type = "zip";
                     break;
     
