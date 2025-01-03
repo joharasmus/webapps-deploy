5b153ac50dfa792e79ac56ae50c6006a2c2ebcfb
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-03 22:06:59 +0100

    trying out addannotation directly

--- src/WebAppDeployer.ts
+++ src/WebAppDeployer.ts
@@ -4,7 +4,6 @@ import * as zipUtility from 'azure-actions-utility/ziputility.js';
 import { addAnnotation } from 'azure-actions-appservice-rest/Utilities/AnnotationUtility';
 import { ActionParameters } from './actionparameters';
 import { AzureAppService } from 'azure-actions-appservice-rest/Arm/azure-app-service';
-import { AzureAppServiceUtility } from 'azure-actions-appservice-rest/Utilities/AzureAppServiceUtility';
 import { Kudu } from 'azure-actions-appservice-rest/Kudu/azure-app-kudu-service';
 import { KuduServiceUtility } from 'azure-actions-appservice-rest/Utilities/KuduServiceUtility';
 import { Package } from "azure-actions-utility/packageUtility";
@@ -15,11 +14,7 @@ export class WebAppDeployer {
     protected actionParams:ActionParameters;
     protected appService: AzureAppService;
     protected kuduService: Kudu;
-    protected appServiceUtility: AzureAppServiceUtility;
     protected kuduServiceUtility: KuduServiceUtility;
-    protected activeDeploymentID: string; 
-    protected applicationURL: string;
-    protected deploymentID: string;
 
     constructor() {
         this.actionParams = ActionParameters.getActionParams();
@@ -35,14 +30,12 @@ export class WebAppDeployer {
         let tempPackagePath = utility.generateTemporaryFolderOrZipPath(`${process.env.RUNNER_TEMP}`, false);
         webPackage = await zipUtility.archiveFolder(webPackage, "", tempPackagePath) as string;
 
-        this.deploymentID = await this.kuduServiceUtility.deployUsingOneDeploy(webPackage, { slotName: "production", commitMessage:"" }, 
+        await this.kuduServiceUtility.deployUsingOneDeploy(webPackage, { slotName: "production", commitMessage:"" }, 
             "", "zip", "true", "true");
     }
 
     public async UpdateDeploymentStatus(isDeploymentSuccess: boolean) {
-        if(!!this.appService) {
-            await addAnnotation(this.actionParams.endpoint, this.appService, isDeploymentSuccess);
-        }
+        await addAnnotation(this.actionParams.endpoint, this.appService, isDeploymentSuccess);
     }
 
     public async initializeForPublishProfile() {
@@ -50,7 +43,5 @@ export class WebAppDeployer {
         
         this.kuduService = publishProfile.kuduService;
         this.kuduServiceUtility = new KuduServiceUtility(this.kuduService);
-        
-        this.applicationURL = publishProfile.appUrl;
     }
 }
