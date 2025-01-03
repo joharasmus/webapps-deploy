53729ff0dd55d1337a7b722a6af23d435c5d0b54
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-03 23:42:09 +0100

    merge webappdeployer into main file

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import * as core from '@actions/core';
 
-import { WebAppDeployer } from './WebAppDeployer';
 import { Package } from 'azure-actions-utility/packageUtility';
+import { SecretParser, FormatType } from 'actions-secret-parser';
+import { Kudu } from 'azure-actions-appservice-rest/Kudu/azure-app-kudu-service';
+import { KuduServiceUtility } from 'azure-actions-appservice-rest/Utilities/KuduServiceUtility';
+import * as utility from 'azure-actions-utility/utility';
+import * as zipUtility from 'azure-actions-utility/ziputility';
 
 export async function main() {
   try {
@@ -9,12 +13,37 @@ export async function main() {
     let packageInput = core.getInput('package');
     let appPackage = new Package(packageInput);
     
-    let deploymentProvider = new WebAppDeployer(publishProfileContent, appPackage);
-    await deploymentProvider.DeployWebApp();
+    await WebAppDeployer.DeployWebApp(publishProfileContent, appPackage);
   }
   catch(error) {
     core.setFailed("Deployment Failed, " + error);
   }
 }
 
+export class WebAppDeployer {
+  
+  public static async DeployWebApp(publishProfileContent: string, appPackage: Package) {
+    
+    let secrets = new SecretParser(publishProfileContent, FormatType.XML);
+
+    let uri = secrets.getSecret("//publishProfile/@publishUrl", false);
+    uri = `https://${uri}`;
+    let username = secrets.getSecret("//publishProfile/@userName", true);
+    let password = secrets.getSecret("//publishProfile/@userPWD", true);
+    
+    let kuduService = new Kudu(uri, { username: username, password: password });
+    let kuduServiceUtility = new KuduServiceUtility(kuduService);
+    
+    let webPackage = appPackage.getPath();
+    
+    await kuduServiceUtility.warmpUp();
+    
+    let tempPackagePath = utility.generateTemporaryFolderOrZipPath(`${process.env.RUNNER_TEMP}`, false);
+    webPackage = await zipUtility.archiveFolder(webPackage, "", tempPackagePath) as string;
+    
+    await kuduServiceUtility.deployUsingOneDeploy(webPackage, { slotName: "production", commitMessage: "" },
+      "", "zip", "true", "true");
+  }
+}
+  
 main();
