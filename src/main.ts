bd1972803e04fced878570288e07c4f6a833183c
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-04 23:30:01 +0100

    final merge

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import * as core from '@actions/core';
+import * as fs from 'node:fs';
 import * as utility from 'azure-actions-utility/utility';
 import * as zipUtility from 'azure-actions-utility/ziputility';
 
 import { Document, DOMParser } from '@xmldom/xmldom';
 import { Package } from 'azure-actions-utility/packageUtility';
-import { Kudu } from './Kudu';
+import { WebClient, WebRequest } from 'azure-actions-webclient/WebClient';
 
 var xPathSelect = require('xpath').select;
 
@@ -31,8 +32,53 @@ export async function main() {
 
   const accessToken = Buffer.from(username + ':' + password).toString('base64');
   
-  let deploymentDetails = await Kudu.oneDeploy(webPackage, uri, accessToken);
+  let deploymentDetails = await oneDeploy(webPackage, uri, accessToken);
   console.log(deploymentDetails);
+}
+
+async function oneDeploy(webPackage: string, scmUri: string, accessToken: string): Promise<any> {
+  
+  let client = new WebClient();
+  
+  let httpRequest: WebRequest = {
+    method: 'POST',
+    uri: scmUri + '/api/publish?async=true&type=zip&clean=true&restart=true',
+    headers: {
+      'Authorization': `Basic ${accessToken}`,
+      'Content-Type': 'application/octet-stream'
+    },
+    body: fs.createReadStream(webPackage)
+  };
+  
+  let response = await client.sendRequest(httpRequest);
+  
+  if (response.statusCode != 202)
+    throw response;
+  
+  let pollableURL: string = response.headers.location;
+  
+  let pollRequest: WebRequest = {
+    method: 'GET',
+    uri: pollableURL,
+    headers: {
+      'Authorization': `Basic ${accessToken}`,
+      'Content-Type': 'application/json; charset=utf-8'
+    }
+  };
+  
+  while (true) {
+    await new Promise(_ => setTimeout(_, 1000));
+    
+    let response = await client.sendRequest(pollRequest);
+    
+    if (response.statusCode == 202)
+      continue;
+    
+    if (response.statusCode == 200)
+      return response.body;
+    
+    throw response; //another statuscode indicates something is wrong
+  }
 }
 
 main();
