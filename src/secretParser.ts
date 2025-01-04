6a2b131c680110510d76e5c31f25556652800d11
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-04 16:51:54 +0100

    simplify getsecret

--- src/secretParser.ts
+++ src/secretParser.ts
@@ -1,49 +1,32 @@
-var core = require('@actions/core');
-var domParser = require('@xmldom/xmldom').DOMParser;
-var xpath = require('xpath');
+import * as core from '@actions/core';
+import { Document, DOMParser } from '@xmldom/xmldom';
+var xpSelect = require('xpath').select;
 
 /**
  * Takes content as string and format type (xml, json).
  * Exposes getSecret method to get value of specific secret in object and set it as secret.
  */
 export class SecretParser {
-    private dom: string;
+    private dom: Document;
 
     constructor(content: string) {
-        this.dom = new domParser().parseFromString(content, "application/xml");
+        this.dom = new DOMParser().parseFromString(content, "application/xml");
     }
 
     /**
-     * @param key jsonpath or xpath depending on content type
+     * @param key xpath
      * @param isSecret should the value parsed be a secret. Deafult: true
-     * @param variableName optional. If provided value will be exported with this variable name
      * @returns a string value or empty string if key not found
      */
-    public getSecret(key: string, isSecret: boolean = true, variableName?: string): string {
-        let value: string = "";
-        value = this.extractXmlPath(key, isSecret, variableName);
-
-        return value;
-    }
-    
-    private extractXmlPath(key: string, isSecret: boolean = false, variableName?: string): string {
-        let value = xpath.select("string(" + key + ")", this.dom);
-        return this.handleSecret(key, value, isSecret, variableName);
-    }
-
-    private handleSecret(key: string, value: string, isSecret: boolean, variableName: string): string {
-        if(!!value) {
-            if(isSecret) {
-                core.setSecret(value);
-            }
-            if(!!variableName) {
-                core.exportVariable(variableName, value);
-            }
-            return value;
-        }
-        else {
-            core.debug("Cannot find key: " + key);
+    public getSecret(key: string, isSecret: boolean = true): string {
+        
+        let xpselectValue = xpSelect("string(" + key + ")", this.dom);
+        if (!xpselectValue)
             return "";
+        if (isSecret) {
+            core.setSecret(xpselectValue)
         }
+
+        return xpselectValue;
     }
 }
