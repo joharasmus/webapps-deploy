b0a7191de868ab87adb299b6b6648bb82ba4871d
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-04 23:24:46 +0100

    dismantled kuduserviceclient

--- src/Kudu.ts
+++ src/Kudu.ts
@@ -1,21 +1,24 @@
 import * as fs from 'fs';
 
-import { KuduServiceClient } from './KuduServiceClient';
-import { WebRequest } from 'azure-actions-webclient/WebClient';
+import { WebClient, WebRequest } from 'azure-actions-webclient/WebClient';
 
 
 export class Kudu {
     public static async oneDeploy(webPackage: string, scmUri: string, accessToken: string): Promise<any> {
 
-        let client = new KuduServiceClient(accessToken);
+        let client2 = new WebClient();
 
         let httpRequest: WebRequest = {
             method: 'POST',
             uri: scmUri + '/api/publish?async=true&type=zip&clean=true&restart=true',
+            headers: {
+                'Authorization': `Basic ${accessToken}`,
+                'Content-Type': 'application/octet-stream'
+            },
             body: fs.createReadStream(webPackage)
         };
 
-        let response = await client.beginRequest(httpRequest, 'application/octet-stream');
+        let response = await client2.sendRequest(httpRequest);
 
         if (response.statusCode != 202)
             throw response;
@@ -24,13 +27,17 @@ export class Kudu {
 
         let pollRequest: WebRequest = {
             method: 'GET',
-            uri: pollableURL
+            uri: pollableURL,
+            headers: {
+                'Authorization': `Basic ${accessToken}`,
+                'Content-Type': 'application/json; charset=utf-8'
+            }
         };
 
         while(true) {
             await new Promise(_ => setTimeout(_, 1000));
     
-            let response = await client.beginRequest(pollRequest);
+            let response = await client2.sendRequest(pollRequest);
             
             if(response.statusCode == 202)
                 continue;
