14fe9f6229eba129f13e3e70554138d1a08f40df
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-04 23:15:22 +0100

    remove retrycount entirely

--- src/KuduServiceClient.ts
+++ src/KuduServiceClient.ts
@@ -16,22 +16,11 @@ export class KuduServiceClient {
         request.headers["Authorization"] = `Basic ${this._accessToken}`
         request.headers['Content-Type'] = contentType || 'application/json; charset=utf-8';
 
-        let retryCount = 1;
-
-        while(retryCount >= 0) {
-            try {
-                return await this._webClient.sendRequest(request);
-            }
-            catch(exception) {
-                let exceptionString: string = exception.toString();
-
-                if(retryCount > 0 && exceptionString.indexOf('Request timeout') != -1) {
-                    retryCount -= 1;
-                    continue;
-                }
-
-                throw exception;
-            }
+        try {
+            return await this._webClient.sendRequest(request);
+        }
+        catch(exception) {
+            throw exception;
         }
 
     }
