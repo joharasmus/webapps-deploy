491d19f4b7008fd33cd926d76f003731c0f16791
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-05 02:03:10 +0100

    try archivefolder without Q

--- src/zipUtility.ts
+++ src/zipUtility.ts
@@ -1,28 +1,17 @@
 
-import * as core from '@actions/core';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
-import Q from 'q';
 import archiver from 'archiver';
 
-export async function archiveFolder(folderPath, targetPath, zipName) {
-    var defer = Q.defer();
-    core.debug('Archiving ' + folderPath + ' to ' + zipName);
-    var outputZipPath = path.join(targetPath, zipName);
-    var output = fs.createWriteStream(outputZipPath);
+export async function archiveFolder(folderPath: string, targetPath: string, zipName: string) {
+
+    let outputZipPath = path.join(targetPath, zipName);
+    let output = fs.createWriteStream(outputZipPath);
+    
     var archive = archiver('zip');
-    output.on('close', function () {
-        core.debug('Successfully created archive ' + zipName);
-        defer.resolve(outputZipPath);
-    });
-
-    output.on('error', function(error) {
-        defer.reject(error);
-    });
-
     archive.pipe(output);
     archive.directory(folderPath, '/');
-    archive.finalize();
+    await archive.finalize();
 
-    return defer.promise;
+    return outputZipPath;
 }
