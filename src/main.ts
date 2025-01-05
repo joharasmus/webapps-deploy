6d089eec58b806aa79d69f3199935ee44edee36b
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-05 13:51:56 +0100

    replaced webclient with httpclient

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import * as core from '@actions/core';
 import * as fs from 'node:fs';
 
 import { Document, DOMParser } from '@xmldom/xmldom';
-import { WebClient } from './WebClient';
 import archiver from 'archiver';
+import { HttpClient } from 'typed-rest-client/HttpClient';
 
 var xPathSelect = require('xpath').select;
 
@@ -32,8 +32,8 @@ export async function main() {
 }
 
 async function oneDeploy(webPackage: string, scmUri: string, accessToken: string): Promise<any> {
-  
-  let client = new WebClient();
+
+  let httpClient = new HttpClient(undefined);
 
   let requestUri = scmUri + '/api/publish?async=true&type=zip&clean=true&restart=true';
   let headers = {
@@ -42,12 +42,12 @@ async function oneDeploy(webPackage: string, scmUri: string, accessToken: string
   };
   let body = fs.createReadStream(webPackage)
   
-  let response = await client.sendRequest('POST', requestUri, headers, body);
+  let response = await httpClient.request('POST', requestUri, body, headers);
   
-  if (response.statusCode != 202)
+  if (response.message.statusCode != 202)
     throw response;
 
-  let pollUri = response.headers.location;
+  let pollUri = response.message.headers.location;
   headers = {
     'Authorization': `Basic ${accessToken}`,
     'Content-Type': 'application/json; charset=utf-8'
@@ -56,13 +56,13 @@ async function oneDeploy(webPackage: string, scmUri: string, accessToken: string
   while (true) {
     await new Promise(_ => setTimeout(_, 1000));
     
-    let response = await client.sendRequest('GET', pollUri, headers);
+    let response = await httpClient.request('GET', pollUri, '', headers);
     
-    if (response.statusCode == 202)
+    if (response.message.statusCode == 202)
       continue;
     
-    if (response.statusCode == 200)
-      return response.body;
+    if (response.message.statusCode == 200)
+      return await response.readBody();
     
     throw response; //another statuscode indicates something is wrong
   }
