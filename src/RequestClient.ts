89d285d67cd4e9c07f10c42cd759464c933e63bd
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-05 13:13:01 +0100

    try without requestoptions

--- src/RequestClient.ts
+++ src/RequestClient.ts
@@ -1,12 +1,9 @@
 import { HttpClient } from "typed-rest-client/HttpClient";
-import { IRequestOptions } from "typed-rest-client/Interfaces";
 
 export class RequestClient {
     public instance: HttpClient;
-    public options: IRequestOptions;
 
     public constructor() {
-        this.options = { };
-        this.instance = new HttpClient(`${process.env.AZURE_HTTP_USER_AGENT}`, undefined, this.options);
+        this.instance = new HttpClient(`${process.env.AZURE_HTTP_USER_AGENT}`, undefined);
     }
 }
