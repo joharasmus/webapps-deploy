4382b8a8d68824d60b33f9a7bcc8441413ad6c4a
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-05 13:34:34 +0100

    remove interface webresponse

--- src/WebClient.ts
+++ src/WebClient.ts
@@ -1,19 +1,12 @@
 
 import { HttpClient } from "typed-rest-client/HttpClient";
 
-export interface WebResponse {
-    statusCode: number;
-    statusMessage: string;
-    headers: any;
-    body: any;
-}
-
 export class WebClient {
     constructor() {
         this._httpClient = new HttpClient(process.env.AZURE_HTTP_USER_AGENT);
     }
 
-    public async sendRequest(method: string, uri: string, headers: any, body?: NodeJS.ReadableStream): Promise<WebResponse> {
+    public async sendRequest(method: string, uri: string, headers: any, body?: NodeJS.ReadableStream): Promise<any> {
 
         let response = await this._httpClient.request(method, uri, body || '', headers);
         let responseBody = await response.readBody();
@@ -23,7 +16,7 @@ export class WebClient {
             statusMessage: response.message.statusMessage,
             headers: response.message.headers,
             body: responseBody
-        } as WebResponse;
+        };
     }
 
     private _httpClient: HttpClient;
