85e6900b542a84aae3d70a3d2c7ceb48c3c174f7
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-07 18:44:25 +0100

    try not setting task.source to anything

--- test.ps1
+++ test.ps1
@@ -5,3 +5,4 @@ git push;
 gh release delete v2 -y --cleanup-tag;  
 gh release create v2 -n "";
 gh workflow run -R 'joharasmus/moreFairStats' 'morefairstats.yml';
+gh run list -R 'joharasmus/moreFairStats' --workflow=morefairstats.yml --json databaseId -q '.[0].databaseId';
