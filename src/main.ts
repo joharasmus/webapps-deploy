d51e51e6b2030d015d5ad5d5de3cb79fd29aaf45
Rasmus Johansson <joha.rasmus@outlook.com>
2025-01-09 18:33:06 +0100

    use allowSyntheticDefaultImports, pt.5

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import * as fs from 'node:fs';
 
 import { Document, DOMParser } from '@xmldom/xmldom';
-const Archiver = require('../lib/core').Archiver;
+const Archiver = require('../lib/core');
 import { HttpClient } from 'typed-rest-client/HttpClient';
 
 const xPathSelect = require('xpath').select;
